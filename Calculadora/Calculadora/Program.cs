8aad4c2f3d1aadfa6f73a10d6855512c3abbb0be
LethicyasLopes <lethicyaslopes@gmail.com>
2022-04-07 17:30:41 -0300

    Efetuado a reestruturação da calculadora

--- Calculadora/Calculadora/Program.cs
+++ Calculadora/Calculadora/Program.cs
@@ -6,34 +6,53 @@ namespace Calculadora
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Calculadora");
-            Console.Write("Digite o primeiro número: ");
-            int num1 = int.Parse(Console.ReadLine());
-
-            Console.Write("Digite o operador: ");
-            string operador = Console.ReadLine();
-
-            Console.Write("Digite o segundo número: ");
-            int num2 = int.Parse(Console.ReadLine());
-
-            switch (operador)
-            {
-                case "+":
-                    Console.WriteLine("Resultado: " + somar(num1, num2));
-                    break;
-                case "-":
-                    Console.WriteLine("Resultado: " + subtrair(num1, num2));
-                    break;
-                case "*":
-                    Console.WriteLine("Resultado: " + multiplicar(num1, num2));
-                    break;
-                case "/":
-                    Console.WriteLine("Resultado: " + dividir(num1, num2));
-                    break;
-            }
-
+            Calculadora();
         }
 
+        public static void Calculadora()
+        {
+            Console.WriteLine("Calculadora");
+            Console.WriteLine("Digite 'ligar' para iniciar a conta");
+            string ligar = Console.ReadLine();
+
+
+            while (ligar.ToUpper() == "LIGAR")
+            {
+
+                Console.Write("Digite o primeiro número: ");
+                int num1 = int.Parse(Console.ReadLine());
+
+                Console.Write("Digite o operador: ");
+                string operador = Console.ReadLine();
+
+                Console.Write("Digite o segundo número: ");
+                int num2 = int.Parse(Console.ReadLine());
+
+                switch (operador)
+                {
+                    case "+":
+                        Console.WriteLine("Resultado: " + somar(num1, num2));
+                        break;
+                    case "-":
+                        Console.WriteLine("Resultado: " + subtrair(num1, num2));
+                        break;
+                    case "*":
+                        Console.WriteLine("Resultado: " + multiplicar(num1, num2));
+                        break;
+                    case "/":
+                        Console.WriteLine("Resultado: " + dividir(num1, num2));
+                        break;
+                }
+
+                Console.WriteLine("Nova operação:");
+                string desligar = Console.ReadLine();
+
+                if (desligar.ToUpper() == "DESLIGAR")
+                {
+                    break;
+                }
+            }
+        }
         public static string somar(int num1, int num2)
         {
             return (num1 + num2).ToString();
